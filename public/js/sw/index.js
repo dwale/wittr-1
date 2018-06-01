ec0e8f653b3d732cba1408b19df5a9d315da5ee3
Rahman Adewale Hafeez <superdwale@gmail.com>
2018-06-01 10:17:02 +0100

    Testing the life cycle of service worker

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,4 @@
 self.addEventListener('fetch', function(event){
-    console.log('The fetched event', event.request);
+    // console.log('The fetched event', event.request);
+    console.log('The hello event');
 });
